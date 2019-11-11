4e9dfdfc8d795d99ee604625f755ac0130da2a76
Xinke Dong <z5158044@ad.unsw.edu.au>
2019-11-11 15:44:37 +1100

    Added history getter and sstter

--- INFS3634GroupProject/app/src/main/java/com/example/infs3634groupproject/History.java
+++ INFS3634GroupProject/app/src/main/java/com/example/infs3634groupproject/History.java
@@ -8,5 +8,60 @@ public class History {
     public int Reps;
     public int oneRM;
 
+    public History(String historyID, String exerciseName, double weight, String date, int reps, int oneRM) {
+        this.historyID = historyID;
+        this.exerciseName = exerciseName;
+        this.weight = weight;
+        this.date = date;
+        Reps = reps;
+        this.oneRM = oneRM;
+    }
 
+    public String getHistoryID() {
+        return historyID;
+    }
+
+    public String getExerciseName() {
+        return exerciseName;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public int getReps() {
+        return Reps;
+    }
+
+    public int getOneRM() {
+        return oneRM;
+    }
+
+    public void setHistoryID(String historyID) {
+        this.historyID = historyID;
+    }
+
+    public void setExerciseName(String exerciseName) {
+        this.exerciseName = exerciseName;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public void setReps(int reps) {
+        Reps = reps;
+    }
+
+    public void setOneRM(int oneRM) {
+        this.oneRM = oneRM;
+    }
 }
