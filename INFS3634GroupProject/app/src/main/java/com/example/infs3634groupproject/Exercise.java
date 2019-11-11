06ba8dcc49d7c6389024b1a0177eb6cb57b36733
Karl Wang <kwang@Karls-MacBook-Pro.local>
2019-11-11 15:25:17 +1100

    Adding Exercise Class

--- INFS3634GroupProject/app/src/main/java/com/example/infs3634groupproject/Exercise.java
+++ INFS3634GroupProject/app/src/main/java/com/example/infs3634groupproject/Exercise.java
@@ -1,4 +1,52 @@
 package com.example.infs3634groupproject;
 
 public class Exercise {
+
+    private int exerciseID;
+
+    private String exerciseName;
+
+    private String exerciseInstructions;
+
+    private int exerciseDrawableID;
+
+
+    public Exercise(int exerciseID, String exerciseName, String exerciseInstructions, int exerciseDrawableID) {
+        this.exerciseID = exerciseID;
+        this.exerciseName = exerciseName;
+        this.exerciseInstructions = exerciseInstructions;
+        this.exerciseDrawableID = exerciseDrawableID;
+    }
+
+    public int getExerciseID() {
+        return exerciseID;
+    }
+
+    public String getExerciseName() {
+        return exerciseName;
+    }
+
+    public String getExerciseInstructions() {
+        return exerciseInstructions;
+    }
+
+    public int getExerciseDrawableID() {
+        return exerciseDrawableID;
+    }
+
+    public void setExerciseID(int exerciseID) {
+        this.exerciseID = exerciseID;
+    }
+
+    public void setExerciseName(String exerciseName) {
+        this.exerciseName = exerciseName;
+    }
+
+    public void setExerciseInstructions(String exerciseInstructions) {
+        this.exerciseInstructions = exerciseInstructions;
+    }
+
+    public void setExerciseDrawableID(int exerciseDrawableID) {
+        this.exerciseDrawableID = exerciseDrawableID;
+    }
 }
