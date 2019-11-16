d3e3fde22cc4ca0c65ea41c80a217c6957722ed6
Xinke Dong <z5158044@ad.unsw.edu.au>
2019-11-16 16:37:00 +1100

    not worked

--- INFS3634GroupProject/app/src/main/java/com/example/infs3634groupproject/HistoryFragment.java
+++ INFS3634GroupProject/app/src/main/java/com/example/infs3634groupproject/HistoryFragment.java
@@ -1,4 +1,18 @@
 package com.example.infs3634groupproject;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-public class HistoryFragment {
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class HistoryFragment extends Fragment{
+    private RecyclerView recyclerView;
+    public HistoryFragment(){
+
+    }
+
+
 }
